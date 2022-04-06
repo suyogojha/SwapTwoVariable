fa8b203efb6e44b5fe96a806956cb505dea54a1b
suyog ojha <ojhasuyog2020@gmail.com>
2022-04-06 11:16:51 +0545

    1st

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapTwoVariable
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 10,  j = 10;
+            SwapValue(i, j);
+            Console.WriteLine($"i={i},j={j}");
+        }
+        public static void SwapValue(int i, int j)
+        {
+            int temp = i;
+            i = j;
+            j = temp;
+            Console.WriteLine($"i={i},j={j}");
+        }
+    }
+}
